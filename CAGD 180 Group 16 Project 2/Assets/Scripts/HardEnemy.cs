3c8ca6b74cf0bd825949c60dab712ae4d87f48a2
PaoXiongCAGD <103204091+PaoXiongCAGD@users.noreply.github.com>
2022-05-12 12:44:52 -0700

    hard enemy
    enemy stays nad the ground and follows the player when it detects it

--- CAGD 180 Group 16 Project 2/Assets/Scripts/HardEnemy.cs
+++ CAGD 180 Group 16 Project 2/Assets/Scripts/HardEnemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 /*Author: Xion, Pao
  * Date: 5/11/2022
  * Description: Makes the hard enemies move towards the player.
@@ -8,22 +9,61 @@ using UnityEngine;
 public class HardEnemy : MonoBehaviour
 {
     //Varibles
-    public float speed;
-    private Transform target;
-    Rigidbody myRb;
+    /* public float speed;
+     private Transform target;
+     Rigidbody myRb;
+     Vector3 moveDirection;
+    */
+     private NavMeshAgent HEnemy;
+    public GameObject Player;
+    public float EnemyRun = 4f;
+    
 
+   
     // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent <Transform> ();
-       
+       // target = GameObject.FindGameObjectWithTag("Player").GetComponent <Transform> ();
+
+        HEnemy = GetComponent<NavMeshAgent>();
+
+        //myRb = GetComponent<Rigidbody>();
+        //target = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Moves the enemy towards the player
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
        
+       //transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+       float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+        if(distance < EnemyRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position - dirToPlayer;
+
+            HEnemy.SetDestination(newPos);
+        }
+      
+      /*
+            Vector3 direction = (target.position - transform.position).normalized;
+            moveDirection = direction;
+
+        
+            myRb.velocity = new Vector3(moveDirection.x, moveDirection.y) * speed;
+        */
+
+
     }
+
+
+    
+  
+
+
+
 }
